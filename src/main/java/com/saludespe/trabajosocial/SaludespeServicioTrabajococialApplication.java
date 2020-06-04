baa45e588db517085b2e54de335c4394fa3bddf5
Anthony <anthoro.53@gmail.com>
2020-06-04 16:42:20 -0500

    First commit

---
+++ src/main/java/com/saludespe/trabajosocial/SaludespeServicioTrabajococialApplication.java
@@ -0,0 +1,13 @@
+package com.saludespe.trabajosocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludespeServicioTrabajococialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludespeServicioTrabajococialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saludespe/trabajosocial/SaludespeServicioTrabajococialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saludespe.trabajosocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludespeServicioTrabajococialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
