c2b9d6c83743278cb69fdaf0928ac67b0c29f65b
8-30 <41347444+8-30@users.noreply.github.com>
2020-06-14 19:20:57 -0500

    Crud NecesidadCapacitacionSocial

--- src/main/java/com/saludespe/trabajosocial/model/dao/INecesidadCapacitacionSocialDAO.java
+++ src/main/java/com/saludespe/trabajosocial/model/dao/INecesidadCapacitacionSocialDAO.java
@@ -1,4 +1,7 @@
 package com.saludespe.trabajosocial.model.dao;
 
-public interface INecesidadCapacitacionSocialDAO {
+import com.saludespe.trabajosocial.model.entities.NecesidadCapacitacionSocial;
+import org.springframework.data.repository.CrudRepository;
+
+public interface INecesidadCapacitacionSocialDAO extends CrudRepository<NecesidadCapacitacionSocial, Long> {
 }
--- src/main/java/com/saludespe/trabajosocial/model/services/implementations/NecesidadCapacitacionSocialService.java
+++ src/main/java/com/saludespe/trabajosocial/model/services/implementations/NecesidadCapacitacionSocialService.java
@@ -1,4 +1,41 @@
 package com.saludespe.trabajosocial.model.services.implementations;
 
-public class NecesidadCapacitacionSocialService {
+import com.saludespe.trabajosocial.model.dao.INecesidadCapacitacionSocialDAO;
+import com.saludespe.trabajosocial.model.entities.IngresoEconomico;
+import com.saludespe.trabajosocial.model.entities.NecesidadCapacitacionSocial;
+import com.saludespe.trabajosocial.model.services.interfaces.INecesidadCapacitacionSocialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NecesidadCapacitacionSocialService implements INecesidadCapacitacionSocialService {
+
+    @Autowired
+    private INecesidadCapacitacionSocialDAO dao;
+
+    @Override
+    @Transactional
+    public NecesidadCapacitacionSocial save(NecesidadCapacitacionSocial necesidadCapacitacionSocial) {
+        return dao.save(necesidadCapacitacionSocial);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) { dao.deleteById(id);}
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<NecesidadCapacitacionSocial> findById(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<NecesidadCapacitacionSocial> findAll() {
+        return (List<NecesidadCapacitacionSocial>)dao.findAll();
+    }
 }
