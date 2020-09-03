bc6228413c5c0a465a6f5abbd5360cfbf2b8b482
daniireinoso <daniireinoso@gmail.com>
2020-09-03 12:29:55 -0500

    feature-seguimiento-acompanamiento-nuevos-cambios

--- src/main/java/com/saludespe/trabajosocial/model/dao/IAspectosImportantesDAO.java
+++
@@ -1,7 +0,0 @@
-package com.saludespe.trabajosocial.model.dao;
-
-import com.saludespe.trabajosocial.model.entities.AspectosImportantes;
-import org.springframework.data.repository.CrudRepository;
-public interface IAsepctosImportantesDAO extends CrudRepository<AspectosImportantes, Long> {
-
-}
